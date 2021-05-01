6e1d11a1b5b3d30291961bc4828329f384f296c8
achanta <achpava@amazon.com>
2021-04-30 22:52:40 -0700

    Longest increasing subsequence

--- src/dp/Main.java
+++ src/dp/Main.java
@@ -2,9 +2,11 @@ package dp;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println(new LCS().run("AGGTAB", "GXTXAYB"));
 		System.out.println(new LCS().run_dp("AGGTAB", "GXTXAYB"));
+		System.out.println(new LIS().run(new int[] {50, 3, 10, 7, 40, 80}));
+		System.out.println(new LIS().run_dp(new int[] {50, 3, 10, 7, 40, 80}));
 	}
 }
