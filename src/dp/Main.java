989a70ec16f9cc908ca266ee01112da5e37826ce
achanta <achpava@amazon.com>
2021-06-10 21:38:46 -0700

    edit distance in dp and recursive

--- src/dp/Main.java
+++ src/dp/Main.java
@@ -8,5 +8,7 @@ public class Main {
 		System.out.println(new LCS().run_dp("AGGTAB", "GXTXAYB"));
 		System.out.println(new LIS().run(new int[] {50, 3, 10, 7, 40, 80}));
 		System.out.println(new LIS().run_dp(new int[] {50, 3, 10, 7, 40, 80}));
+		System.out.println(new EditDistance().run("sunday", "saturday"));
+		System.out.println(new EditDistance().run_dp("sunday", "saturday"));
 	}
 }
